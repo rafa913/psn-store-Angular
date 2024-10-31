fa79f817ef08e7d15e724dcd4b5680216e3cb68a
rafa913 <rafaellacerda60@gmail.com>
2024-10-31 12:44:22 -0400

    primeiro commit ajustes iniciais de menu e card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { CardsComponent } from './components/cards/cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuBarComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
